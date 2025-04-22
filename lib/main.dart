0dbc46b5ed54a47f43c589f80f6df7d9595d7a4c
sahil-sahu <sahilku2003@gmail.com>
2025-04-22 07:56:19 +0530

    integrated firebase + with realtime temperature

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:safetyjacket_app/health_monitor_page.dart';
-
-void main() {
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(MyApp());
 }
 
